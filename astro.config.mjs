b38aa4c36711760e7b6010b58b27aea3bf25846b
bumu <bumu@airdb.io>
2023-11-03 00:52:38 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,51 +20,40 @@ export default defineConfig({
 						{ label: 'Today', link: '/guides/today', badge: 'New' },
 					],
 				},
-				{
-					label: 'Cloud-Native',
-					collapsed: true,
-					items: [
-						{
-							label: 'docker', items: [
-								{ label: 'Example Guide', link: '/cloud-native/docker/readme' , badge: 'New',},
-								{ label: 'Example Guide 2', link: '/cloud-native/docker/compose', badge: { text: 'Experimental', variant: 'caution' }, },
-							]
-						},
-						{
-							label: 'kube', items: [
-								{ label: 'Example Guide', link: '/cloud-native/kube/readme' },
-								{ label: 'Example Guide 2', link: '/cloud-native/kube/1-kubernetes-on-macos' },
-								{ label: 'Example Guide 3', link: '/cloud-native/kube/helm' },
-								{ label: 'Example Guide 5', link: '/cloud-native/kube/9-faq' },
-								{ label: 'Example Guide 6', link: '/cloud-native/kube/2-ingress-exmaple' },
-								{ label: 'Example Guide 7', link: '/cloud-native/kube/5-helm' },
-							]
-						},
-					]
-				},
-				{
-					label: 'AI', collapsed: true, items: [
-						{ label: 'Example Guide', link: '/ai/aigc/' },
-						{ label: 'Example Guide', link: '/ai/algorithm/' },
-						{ label: 'Example Guide', link: '/ai/fine-tune/' },
-						{ label: 'Example Guide', link: '/ai/awesome' },
-
-					]
-				},
-				{ label: 'Arch', collapsed: true, autogenerate: { directory: 'arch' },},
-				{ label: 'Academy', collapsed: true, autogenerate: { directory: 'academy' },},
-				{ label: 'DevOps', collapsed: true, autogenerate: { directory: 'devops' },},
+				{ label: 'AI', collapsed: true, autogenerate: { directory: 'ai' },},
+				{ label: 'Blockchain', collapsed: true, autogenerate: { directory: 'blockchain' },},
+				{ label: 'Cloud Computing', collapsed: true, autogenerate: { directory: 'blockchain' }, },
+				{ label: 'Data', collapsed: true, autogenerate: { directory: 'bigdata' }, },
+				{ label: 'Energy', collapsed: true, autogenerate: { directory: 'energy' }, },
 				{ label: 'Fintech', collapsed: true, autogenerate: { directory: 'fintech' },},
-				{ label: 'Fundation', collapsed: true,	autogenerate: { directory: 'fund' },},
-				{ label: 'Interview', collapsed: true, autogenerate: { directory: 'interview' },},
-				{ label: 'Team Management', collapsed: true, autogenerate: { directory: 'mgmt' },},
-				{ label: 'Network', collapsed: true, autogenerate: { directory: 'network' },},
-				{ label: 'OS',collapsed: true, autogenerate: { directory: 'os' },},
-				{ label: 'Gateway', collapsed: true, autogenerate: { directory: 'gateway' },},
 				{ label: 'Security', collapsed: true, autogenerate: { directory: 'security' },},
-				{ label: 'Style', collapsed: true, autogenerate: { directory: 'style' }, },
-				{ label: 'Contribution', collapsed: true, autogenerate: { directory: 'contribution' }, },
-				{ label: 'About', collapsed: true, autogenerate: { directory: 'about' }, }
+				{ label: 'DevOps', collapsed: true, autogenerate: { directory: 'devops' },},
+				{
+					label: 'Team', collapsed: true,
+					items: [
+						{ label: 'Team Management', collapsed: true, autogenerate: { directory: 'mgmt' },},
+						{ label: 'Culture', collapsed: true, autogenerate: { directory: 'culture' },},
+						{ label: 'Academy', collapsed: true, autogenerate: { directory: 'academy' },},
+						{ label: 'Interview', collapsed: true, autogenerate: { directory: 'interview' },},
+						{ label: 'Style', collapsed: true, autogenerate: { directory: 'style' }, },
+					],
+				},
+				{
+					label: 'IaaS', collapsed: true,
+						items: [
+						{ label: 'Network', collapsed: true, autogenerate: { directory: 'network' },},
+						{ label: 'OS',collapsed: true, autogenerate: { directory: 'os' },},
+						{ label: 'Gateway', collapsed: true, autogenerate: { directory: 'gateway' },},
+					],
+				},
+				{
+					label: 'About', collapsed: true,
+						items: [
+							{ label: 'Contribution', collapsed: true, autogenerate: { directory: 'contribution' }, },
+							{ label: 'Fundation', collapsed: true,	autogenerate: { directory: 'fund' },},
+							{ label: 'About', collapsed: true, autogenerate: { directory: 'about' }, }
+					],
+				},
 			],
 		}),
 	],
---
+++ src/content/docs/mgmt/culture/change_is_always_better.md
@@ -0,0 +1,6 @@
+---
+title: Change is Always Better
+description: change is always better
+---
+
+Change is Always Better (改变永远是最好的)
