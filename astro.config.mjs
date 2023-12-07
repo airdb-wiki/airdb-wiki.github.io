ab5cee044a34d8faecbbff0e2b23c5efa0de42a8
bumu <bumu@airdb.io>
2023-12-08 00:28:23 +0800

    upgrade to astro 4.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Start Here',
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Guide', link: '/guides/example', badge: 'Welcome' },
